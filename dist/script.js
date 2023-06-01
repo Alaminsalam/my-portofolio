816a2f65fa2c8096bee0bd4bc0da14dec3574d0a
Alaminsalam <90309888+Alaminsalam@users.noreply.github.com>
2023-06-01 23:25:11 +0800

    update

--- dist/script.js
+++ dist/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function() {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
     if(window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.remove('remove');
+        toTop.classList.add('hidden');
     }
 };
 
@@ -14,7 +19,24 @@ window.onscroll = function() {
 
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
-hamburger.addEventListener ('click', function(){
+hamburger.addEventListener ('click', function (){
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
+});
+
+// klik diluar untuk menghilangkan hamburger
+
+window.addEventListener('click', function (e) {
+    if(e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+});
+
+// Darkmode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function (){
+    darkToggle.checked ? html.classList.add('dark') : html.classList.remove('dark');
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   theme: {
     extend: {
       container : {
@@ -9,12 +10,13 @@ module.exports = {
       },
       colors : {
         primary : '#4f46e5',
-        secondary : '#262626'
+        secondary : '#262626',
+        dark: '#0f172a',
       },
       screems: {
         '2xl' : '1320px'
       }
     },
   },
-  plugins: [],
+  plugins: []
 }
